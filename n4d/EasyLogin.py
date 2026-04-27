8041f781c8feebfe50536f8f3dced2f3d860633a
Raul Rodrigo Segura <raurodse@gmail.com>
2026-04-27 14:10:25 +0200

    initial release

---
+++ n4d/EasyLogin.py
@@ -0,0 +1,153 @@
+import bcrypt
+import bson
+from pathlib import Path
+from yaml import safe_load
+import n4d.responses
+from time import time
+from random import randrange
+
+class EasyLogin:
+    USER_NOT_FOUND = -1
+    PASSWORD_INVALID = -2
+    USER_NOT_IN_CACHE = -10
+    USER_CACHE_EXPIRED = -11
+    WRONG_SAVE = -30
+    SERVER_UNREACHABLE = -50
+
+    def __init__(self) -> None:
+        self.load_default_paths()
+        self.load_config()
+
+    def load_default_paths(self):
+        self.config_path = Path("/etc/easylogin/config.yaml")
+        self.db_path = Path("/var/lib/easylogin/shadow.db")
+
+    def load_config(self) -> None:
+        self.config = safe_load(self.config_path.read_text()) if self.config_path.exists() else { "initaial_uid": 70000 }
+
+    def validate_id_user(self, username, password) -> n4d.responses:
+        user = self.load_user(username.split("@")[0])
+        if user is None:
+            return n4d.responses.build_failed_call_response(EasyLogin.USER_NOT_FOUND)
+        result = password == user["hash"]
+        if result:
+            user.pop("hash", None)
+            return n4d.responses.build_successful_call_response(user["info"])
+        return n4d.responses.build_failed_call_response(EasyLogin.PASSWORD_INVALID)
+
+    def store_id_user(self, username, info):
+       
+        user = {
+                "login":".easy",
+                "name":"",
+                "surname":"",
+                "home":"/home/",
+                "shell":"/bin/bash",
+                "uid": 70000,
+                "group": "students"
+                }
+        user["login"] = user["login"] + info["login"]
+        user["home"] = user["home"] + info["login"]
+        
+        if "name" in info:
+            user["name"] = info["name"]
+        if "surname" in info:
+            user["surname"] = info["surname"]
+        if "login" in info:
+            user["login"] = info["login"] + user["login"].lower()
+        else:
+            aux = ""
+            if "name" in info:
+                aux = info["name"][0:3]
+            if "surname" in info:
+                aux = aux + info["surname"][0:3]
+            if aux == "":
+                from random import randrange
+                aux = ''.join([chr(randrange(97, 123)) for i in range(6)])
+            user["login"] = aux + user["login"].lower()
+        user["home"] = user["home"] + user["login"]
+
+        user["uid"] = self.get_next_uid()
+
+        if user is not None:
+            user_info = {}
+            user_info['info'] = user
+            pass_hash = "" 
+            user_info["hash"] = pass_hash
+            if self.save_info(username, user_info):
+                return n4d.responses.build_successful_call_response(True)
+        return n4d.responses.build_failed_call_response(EasyLogin.WRONG_SAVE)
+
+
+    def save_info(self, username, info):
+        self.exists_or_build_db()
+        try:
+            with self.db_path.open("br") as fd:
+                users_db = bson.decode(fd.read())
+        except Exception:
+            users_db = {}
+        try:
+            login = username
+            patch_info = str(info['info'])
+            info["info"] = patch_info 
+            users_db[login] = info
+            with self.db_path.open("bw") as fd:
+                fd.write(bson.encode(users_db))
+            return True
+        except Exception:
+            return False
+
+    def remove_entry(self, username):
+        self.exists_or_build_db()
+        try:
+            with self.db_path.open("br") as fd:
+                cache = bson.decode(fd.read())
+            if username in cache:
+                del cache[username]
+            with self.db_path.open("bw") as fd:
+                fd.write(bson.encode(cache))
+            return True
+        except Exception:
+            return False
+
+    def load_user(self, username):
+        if not self.db_path.exists():
+            return None
+        if self.db_path.stat().st_size == 0:
+            return None
+        with self.db_path("br") as fd:
+            cache = bson.decode(fd.read())
+        if username in cache:
+            return cache[username]
+        return None
+
+    def get_next_uid(self):
+        self.exists_or_build_db()
+        try:
+            with self.db_path.open("br") as fd:
+                users_db = bson.decode(fd.read())
+        except Exception:
+            users_db = {}
+        if len(users_db) == 0:
+            return self.config["initaial_uid"]
+        max_uid = max([user["info"]["uid"] for user in users_db.values()])
+        return max_uid + 1
+
+    def exists_or_build_db(self):
+        if not self.db_path.parent.exists():
+            self.db_path.parent.mkdir(parents=True,
+                                         exist_ok=True,
+                                         mode=0o700)
+        if not self.db_path.exists():
+            self.db_path.touch(mode=0o600)
+            self._wipe_db()
+        return True
+
+    def wipe_db(self):
+        self.exists_or_build_db()
+        self._wipe_db()
+        return n4d.responses.build_successful_call_response(True)
+
+    def _wipe_db(self):
+        with self.db_path.open("bw") as fd:
+            fd.write(bson.encode({}))
