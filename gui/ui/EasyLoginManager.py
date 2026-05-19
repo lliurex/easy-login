86378c647aefab4e5daba2460f92bafe44f77c39
jrpelegrina <juapesai@hotmail.com>
2026-05-19 11:17:04 +0200

    WIP in GUI

--- gui/ui/EasyLoginManager.py
+++ gui/ui/EasyLoginManager.py
@@ -284,7 +284,7 @@ class EasyLoginManager(object):
 		info={}
 		info["name"]=dataToSave.get("name")
 		info["surname"]=dataToSave.get("surname")
-		info["login"]=dataToSave.get("login")
+		info["login"]=dataToSave.get("login").replace(".easy",'')
 		info["uid"]=dataToSave.get("uid")
 
 		if username!=usernameOrig:
